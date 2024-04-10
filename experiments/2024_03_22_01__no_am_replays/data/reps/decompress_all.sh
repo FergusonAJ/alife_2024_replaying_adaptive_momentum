0635b3109da59e9a75dcc08e45aac2da2966b03b
Austin Ferguson <ferguson.austin.j@gmail.com>
2024-04-10 12:09:25 -0400

    Remove old dirs without leading zeros

---
+++ experiments/2024_03_22_01__no_am_replays/data/reps/decompress_all.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+THIS_DIR=$(pwd)
+
+DIR_LIST=$(ls | grep -oP "\d\d\d\d\d")
+
+for DIR in ${DIR_LIST}
+do
+  cd ${DIR}
+  echo "Decompressing file in ${DIR}"
+  tar -xzf combined_replay_cross_data.tar.gz
+  if [ -s combined_shuffled_replay_cross_data.tar.gz ] 
+  then
+    echo "  Found shuffled replay data, decompressing..."
+    tar -xzf combined_shuffled_replay_cross_data.tar.gz
+  fi
+  if [ -s combined_selected_replay_cross_data.tar.gz ] 
+  then
+    echo "  Found selected replay data, decompressing..."
+    tar -xzf combined_selected_replay_cross_data.tar.gz
+  fi
+  cd ${THIS_DIR}
+done
+
