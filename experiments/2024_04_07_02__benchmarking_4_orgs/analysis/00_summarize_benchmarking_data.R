8e23bab2ed883e67882cf654886dd2bf39d8ab66
Austin Ferguson <ferguson.austin.j@gmail.com>
2024-04-10 12:12:47 -0400

    Update 4 org benchmark analyses

--- experiments/2024_04_07_02__benchmarking_4_orgs/analysis/00_summarize_benchmarking_data.R
+++ experiments/2024_04_07_02__benchmarking_4_orgs/analysis/00_summarize_benchmarking_data.R
@@ -3,6 +3,7 @@ rm(list = ls())
 library(dplyr)
 
 df_all = read.csv('../data/combined_cross_info.csv')
+colnames(df_all) = c('slurm_task_id', 'trial_id', 'leading_edge_index', 'leading_edge_val', 'relative_update', 'cross_counter')
 num_trials = 10000
 
 df_summary = df_all[df_all$cross_counter == 1,] %>% dplyr::group_by(leading_edge_index, leading_edge_val) %>% dplyr::summarize(count = dplyr::n())
